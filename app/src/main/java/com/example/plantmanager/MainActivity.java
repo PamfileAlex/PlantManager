8c64c7fd87be0dd995a375daefe5514467c246e4
RobertaLuca <robertaiulianaluca@yahoo.com>
2021-10-23 11:19:03 +0300

    Created empty activity project

---
+++ app/src/main/java/com/example/plantmanager/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.plantmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
