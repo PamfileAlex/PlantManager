7075a2b7720c7c9772a2a46c48e828fb80c0ffca
Pamfile Alex <damrosrak@gmail.com>
2021-10-23 13:53:31 +0300

    Resolved Databinding and added basic login UI

--- app/src/main/java/com/example/plantmanager/LoginFragment.java
+++ app/src/main/java/com/example/plantmanager/LoginFragment.java
@@ -3,24 +3,24 @@ package com.example.plantmanager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.example.plantmanager.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        FragmentLoginBinding binding = FragmentLoginBinding.inflate(inflater, container,false);
 
-        final TextView textView = (TextView)view.findViewById(R.id.textView);
-        textView.setOnClickListener(view1 -> {
-            Navigation.findNavController(view).navigate(R.id.navigate_from_loginFragment_to_registerFragment);
-        });
-        return view;
+        binding.btnLogin.setOnClickListener(view -> NavHostFragment.findNavController(LoginFragment.this)
+                .navigate(R.id.navigate_from_loginFragment_to_registerFragment));
+
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/plantmanager/RegisterFragment.java
+++ app/src/main/java/com/example/plantmanager/RegisterFragment.java
@@ -3,24 +3,24 @@ package com.example.plantmanager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.example.plantmanager.databinding.FragmentRegisterBinding;
 
 public class RegisterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        FragmentRegisterBinding binding = FragmentRegisterBinding.inflate(inflater, container,false);
 
-        final TextView textView = (TextView)view.findViewById(R.id.textView2);
-        textView.setOnClickListener(view1 -> {
-            Navigation.findNavController(view).navigate(R.id.navigate_from_registerFragment_to_loginFragment);
-        });
-        return view;
+        binding.textView2.setOnClickListener(view -> NavHostFragment.findNavController(RegisterFragment.this)
+                .navigate(R.id.navigate_from_registerFragment_to_loginFragment));
+
+        return binding.getRoot();
     }
 }
