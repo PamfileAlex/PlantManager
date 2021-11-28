e7acc1d85b54e7c035c2c2fa04f92a1ad1f7b6c7
RobertaLuca <robertaiulianaluca@yahoo.com>
2021-11-28 16:42:49 +0200

    Changed PreparedStatement to CallableStatement Added database script

--- app/src/main/java/com/example/plantmanager/database/UserManager.java
+++ app/src/main/java/com/example/plantmanager/database/UserManager.java
@@ -4,7 +4,6 @@ import com.example.plantmanager.models.User;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -13,15 +12,17 @@ public class UserManager {
     public ArrayList<User> getUsers(Connection connection) throws SQLException {
         ArrayList<User> users = new ArrayList<>();
 
-        PreparedStatement statement = connection.prepareStatement("EXEC selectAllUsers");
-        ResultSet rs = statement.executeQuery();
+        CallableStatement statement = connection.prepareCall("{call spSelectUsers}");
+        statement.execute();
 
-        while (rs.next()) {
-            User user = new User(rs.getString("last_name"),
-                    rs.getString("first_name"),
-                    rs.getString("email"),
-                    rs.getString("username"),
-                    rs.getString("password"));
+        ResultSet resultSet = statement.getResultSet();
+
+        while (resultSet.next()) {
+            User user = new User(resultSet.getString("last_name"),
+                    resultSet.getString("first_name"),
+                    resultSet.getString("email"),
+                    resultSet.getString("username"),
+                    resultSet.getString("password"));
             users.add(user);
         }
 
@@ -29,12 +30,14 @@ public class UserManager {
     }
 
     public void insertUser(Connection connection, User user) throws SQLException {
-        CallableStatement statement = connection.prepareCall("{call insertUser(?, ?, ?, ?, ?)}");
+        CallableStatement statement = connection.prepareCall("{call spInsertUser(?, ?, ?, ?, ?)}");
+
         statement.setString(1, user.getLastName());
         statement.setString(2, user.getFirstName());
         statement.setString(3, user.getEmail());
         statement.setString(4, user.getUsername());
         statement.setString(5, user.getPassword());
+        
         statement.execute();
     }
 }
