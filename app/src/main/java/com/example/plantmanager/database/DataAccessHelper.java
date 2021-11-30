f2d75066dba62b8a28bcb714f6921fa90cfb64e3
Pamfile Alex <damrosrak@gmail.com>
2021-11-30 21:09:21 +0200

    Added my connection string to DataAccessHelper

--- app/src/main/java/com/example/plantmanager/database/DataAccessHelper.java
+++ app/src/main/java/com/example/plantmanager/database/DataAccessHelper.java
@@ -2,5 +2,8 @@ package com.example.plantmanager.database;
 
 public class DataAccessHelper {
     final static String dbClasses = "net.sourceforge.jtds.jdbc.Driver";
-    final static String dbConnectionUrl ="jdbc:jtds:sqlserver://172.27.160.1:1433;databaseName=PlantManager;user=sa;password=1234;instance=SQLEXPRESS;";
+    // ROBERTA
+    //final static String dbConnectionUrl ="jdbc:jtds:sqlserver://172.27.160.1:1433;databaseName=PlantManager;user=sa;password=1234;instance=SQLEXPRESS;";
+    // ALEX
+    final static String dbConnectionUrl ="jdbc:jtds:sqlserver://192.168.1.139:1433;databaseName=PlantManager;user=admin;password=1q2w3e;instance=SQLEXPRESS;";
 }
