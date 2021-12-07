e1a5e7f35084a31c5dc80f51d3bef9f1b34f8815
RobertaLuca <robertaiulianaluca@yahoo.com>
2021-12-07 12:02:04 +0200

    Added option for taking photo from gallery

--- app/src/main/java/com/example/plantmanager/fragments/AddPlantFragment.java
+++ app/src/main/java/com/example/plantmanager/fragments/AddPlantFragment.java
@@ -1,19 +1,19 @@
 package com.example.plantmanager.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.provider.MediaStore;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +27,14 @@ public class AddPlantFragment extends Fragment {
 
     FragmentAddPlantBinding binding;
     ApplicationViewModel applicationViewModel;
+
+    private enum Options {
+        CAMERA,
+        GALLERY,
+        NONE
+    }
+
+    private Options option;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,19 +51,53 @@ public class AddPlantFragment extends Fragment {
         SpinnerHelper.populateSpinnerWithCategories(spinner, getContext(), applicationViewModel.getCategories());
 
         binding.btnAddImage.setOnClickListener(view -> {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            someActivityResultLauncher.launch(intent);
+            selectImage();
         });
         return binding.getRoot();
     }
 
-    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == Activity.RESULT_OK) {
-
-                    Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
-                    binding.image.setImageBitmap(bitmap);
+                    Intent intent = result.getData();
+                    switch (option){
+                        case CAMERA:
+                            Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
+                            binding.image.setImageBitmap(bitmap);
+                            break;
+                        case GALLERY:
+                            binding.image.setImageURI(intent.getData());
+                            break;
+                        case NONE:
+                            break;
+                    }
+                    option = Options.NONE;
                 }
             });
+
+    private void selectImage() {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery", "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, (dialog, item) -> {
+            if (options[item].equals("Take Photo"))
+            {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                option = Options.CAMERA;
+                activityResultLauncher.launch(intent);
+            }
+            else if (options[item].equals("Choose from Gallery"))
+            {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                option = Options.GALLERY;
+                activityResultLauncher.launch(intent);
+            }
+            else if (options[item].equals("Cancel")) {
+                option = Options.NONE;
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
 }
